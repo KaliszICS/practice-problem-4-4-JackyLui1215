213844f7b45f31636dff3705693b92b565731597
JackyLui1215 <343344461@tdsb.ca>
2026-04-13 15:34:55 +0000

    finished

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,32 @@
+/**
+	* File: Lesson 4.4 - Boolean functions with loops
+	* Author: Jacky Lui
+	* Date Last Modified: April 13, 2023
+	*/
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
+
+	public static boolean hasCapital(String word) {
+			if (word.toLowerCase() == word) {
+				return false;
+			}
+        return true;
+		}
+
+	public static boolean isPrime(int number) {
+		if (number == 1 ) {
+				return false;
+			}
+		for (int i = 2; i < number; i++) {
+			if (number % i == 0){
+				return false;
+			}
+		}
+		return true;
+	}
+
 
 }
